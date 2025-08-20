209fea1c419d82458895475cfd082712c429f251
joren-bulahan <145977667+joren-bulahan@users.noreply.github.com>
2025-08-20 14:24:19 +0800

    Add files via upload

---
+++ Lecture1/first_letter.py
@@ -0,0 +1,67 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "4ae56f9a-b2eb-43ec-87c1-bc39835d0279",
+   "metadata": {},
+   "source": [
+    "Review Questions 4"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "5a0bff54-2841-4566-9add-233c7cecf51b",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Tell me your name:  zooren\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Z\n"
+     ]
+    }
+   ],
+   "source": [
+    "first = (input(\"Tell me your name: \"))\n",
+    "print(first.upper()[0])\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "43b669f6-52a2-42e1-83a0-90f5f6800924",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:myenv]",
+   "language": "python",
+   "name": "conda-env-myenv-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.18"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ Lecture1/translate.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "b5d35dc7-fbfe-432e-9741-03799c692de9",
+   "metadata": {},
+   "source": [
+    "Review Questions 7"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 19,
+   "id": "8b1f8292-4561-458f-a5f3-b0f870cf9d4f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter some text:  : I like to eat eggs and spam.\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Leetscode:  : I 1ik3 70 347 3gg5 4nd 5p4m.\n"
+     ]
+    }
+   ],
+   "source": [
+    "first = (input(\"Enter some text: \"))\n",
+    "first = first.replace(\"a\",\"4\")\n",
+    "first = first.replace(\"b\",\"8\")\n",
+    "first = first.replace(\"e\",\"3\")\n",
+    "first = first.replace(\"l\",\"1\")\n",
+    "first = first.replace(\"o\",\"0\")\n",
+    "first = first.replace(\"s\",\"5\")\n",
+    "first = first.replace(\"t\",\"7\")\n",
+    "print(\"Leetscode: \", first)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5225a16a-428c-4f7b-8563-46d7869562ca",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:myenv]",
+   "language": "python",
+   "name": "conda-env-myenv-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.18"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
